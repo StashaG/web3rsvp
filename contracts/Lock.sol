0653ae9942af28fd5b906d15e2e4835a1a351b99
Stacey <stacey.graham1106@gmail.com>
2022-08-04 23:55:21 -0400

    Initial commit

---
+++ contracts/Lock.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract Web3RSVP {
+    struct CreateEvent {
+        bytes32 eventId;
+        string eventDataCID;
+        address eventOwner;
+        uint256 eventTimestamp;
+        uint256 deposit;
+        uint256 maxCapacity;
+        address[] confirmedRSVPs;
+        address[] claimedRSVPs;
+        bool paidOut;
+    }
+
+    mapping(bytes32 => CreateEvent) public idToEvent;
+    
+}
